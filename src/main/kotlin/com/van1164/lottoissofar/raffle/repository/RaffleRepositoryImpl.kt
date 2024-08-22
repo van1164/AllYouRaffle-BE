1ba23264cf72ec5200ded6c40a7fab1d81922b26
dfdrdodm95 <dfdrdodm95@gmail.com>
2024-08-22 22:19:53 +0900

    chore: 구현체 작성용 주석 추가

--- src/main/kotlin/com/van1164/lottoissofar/raffle/repository/RaffleRepositoryImpl.kt
+++ src/main/kotlin/com/van1164/lottoissofar/raffle/repository/RaffleRepositoryImpl.kt
@@ -1,7 +1,33 @@
 package com.van1164.lottoissofar.raffle.repository
 
+import com.van1164.lottoissofar.common.domain.Raffle
 import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 
 class RaffleRepositoryImpl {
+/*
+    @Query("select r from Raffle r where r.status = 'ACTIVE'")
+    fun findAllByStatusIsACTIVE(): List<Raffle> {
+    }
 
+    @Query("select r from Raffle r where r.status = 'ACTIVE' and r.isFree = true")
+    fun findAllByStatusIsACTIVEAndFree(): List<Raffle> {
+
+    }
+
+    @Query("select r from Raffle r where r.status = 'ACTIVE' and r.isFree = false")
+    fun findAllByStatusIsACTIVEAndNotFree(): List<Raffle> {
+
+    }
+
+    @Query("select r from Raffle r where r.status = 'ACTIVE' and r.isFree = false and r.id = :raffleId")
+    fun findByStatusIsACTIVEAndNotFree(@Param(value= "raffleId") raffleId : Long): Raffle? {
+
+    }
+
+    @Query("select r from Raffle r where r.status = 'ACTIVE' and r.isFree = true and r.id = :raffleId")
+    fun findByStatusIsACTIVEAndFree(@Param(value = "raffleId") raffleId : Long): Raffle? {
+
+    }
+ */
 }
