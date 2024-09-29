98ac65577d0a667512f73f90bce26effcfce71b0
Sihwan Kim <vkn123@naver.com>
2024-09-30 01:16:16 +0900

    fix: 래플 조회 롤백

--- src/main/kotlin/com/van1164/lottoissofar/raffle/controller/RaffleController.kt
+++ src/main/kotlin/com/van1164/lottoissofar/raffle/controller/RaffleController.kt
@@ -8,6 +8,7 @@ import com.van1164.lottoissofar.common.security.CustomUserDetails
 import com.van1164.lottoissofar.raffle.service.RaffleService
 import io.swagger.v3.oas.annotations.Parameter
 import org.springframework.data.domain.Page
+import org.springframework.data.domain.PageRequest
 import org.springframework.data.domain.Pageable
 import org.springframework.data.web.PageableDefault
 import org.springframework.http.ResponseEntity
@@ -71,10 +72,14 @@ class RaffleController(
 
     @GetMapping("/active/free")
     fun getActiveFree(
-        @PageableDefault pageable: Pageable
-    ): Page<Raffle> {
-        return raffleService.getActiveFreeRaffle(pageable)
+    ): List<Raffle> {
+        val page = 0 // 페이지 번호 (0부터 시작)
+        val size = 10 // 페이지당 아이템 개수
+        val pageable: Pageable = PageRequest.of(page, size)
+        return raffleService.getActiveFreeRaffle(pageable = pageable).toList()
     }
+
+
 
     @GetMapping("/active/not_free/detail/{raffleId}")
     fun getDetailNotFree(
